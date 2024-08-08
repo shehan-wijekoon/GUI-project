412dd28745421178b235eb921bb735391c08e7b8
shehan-wijekoon <168211852+shehan-wijekoon@users.noreply.github.com>
2024-08-08 14:33:26 +0530

    design the calculate.cs form

--- GUIproject/calculate.Designer.cs
+++ GUIproject/calculate.Designer.cs
@@ -28,12 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "calculate";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            button1 = new Button();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label10 = new Label();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(234, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Salary calculation";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(373, 61);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 20);
+            label3.TabIndex = 2;
+            label3.Text = "ID";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 111);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Department";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(373, 111);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Hours worked";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 166);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 20);
+            label6.TabIndex = 5;
+            label6.Text = "OT Hours";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(373, 166);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Bonuses";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 311);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 40);
+            button1.TabIndex = 7;
+            button1.Text = "Calculate";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(24, 272);
+            label8.Name = "label8";
+            label8.Size = new Size(114, 20);
+            label8.TabIndex = 8;
+            label8.Text = "Calculate Salary";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(397, 286);
+            label9.Name = "label9";
+            label9.Size = new Size(84, 20);
+            label9.TabIndex = 9;
+            label9.Text = "Final Salary";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(67, 58);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(397, 58);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(480, 108);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(442, 163);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(88, 166);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(299, 318);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(306, 27);
+            textBox7.TabIndex = 16;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(260, 321);
+            label10.Name = "label10";
+            label10.Size = new Size(24, 20);
+            label10.TabIndex = 17;
+            label10.Text = "Rs";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Software Development", "Management", "IT support and Consulton", "QA Team", "UI/UX" });
+            comboBox1.Location = new Point(107, 108);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 18;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // calculate
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(682, 433);
+            Controls.Add(comboBox1);
+            Controls.Add(label10);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "calculate";
+            Text = "calculate";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button button1;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label10;
+        private ComboBox comboBox1;
     }
 }
--- GUIproject/calculate.cs
+++ GUIproject/calculate.cs
@@ -16,5 +16,10 @@ namespace GUIproject
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

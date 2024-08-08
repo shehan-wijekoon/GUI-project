1852b197e29038f2ed841c2b0e135685eb9e5035
shehan-wijekoon <168211852+shehan-wijekoon@users.noreply.github.com>
2024-08-08 14:05:32 +0530

    make new changes to form

--- GUIproject/Form1.Designer.cs
+++ GUIproject/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            Btncalculate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(186, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(430, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Employee Management System";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Location = new Point(186, 79);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(684, 462);
+            panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(35, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Employee salary";
+            // 
+            // Btncalculate
+            // 
+            Btncalculate.Location = new Point(12, 131);
+            Btncalculate.Name = "Btncalculate";
+            Btncalculate.Size = new Size(168, 56);
+            Btncalculate.TabIndex = 0;
+            Btncalculate.Text = "CALCULATE";
+            Btncalculate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(882, 553);
+            Controls.Add(Btncalculate);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Home";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private Button Btncalculate;
     }
 }
